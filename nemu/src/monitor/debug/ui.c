e315c90c0f21cd2214598ce1a924940666301d97
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 15:08:38 +0000

    >  compile NEMU 3019244287 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:08:38 up 3 days,  8:17,  3 users,  load average: 0.38, 0.20, 0.12 874f114f516789751e58eef9f705afd81330cf71

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -42,6 +42,8 @@ static int cmd_si(char *args);
 
 static int cmd_info(char *args);
 
+static int cmd_x(char *args);
+
 static struct {
 	char *name;
 	char *description;
@@ -52,6 +54,7 @@ static struct {
 	{ "q", "Exit NEMU", cmd_q },
         { "si", "Single Step", cmd_si},
         { "info", "Print the state of CPU or monitor", cmd_info},
+        { "x", "read memory", cmd_x},
 
 	/* TODO: Add more commands */
 
@@ -117,6 +120,30 @@ static int cmd_info(char *args)
   }
   return 0;
 }
+
+static int cmd_x(char *args)
+{
+  char *N = strtok(NULL, " ");
+  uint32_t n = 0;
+  sscanf(N,"%d",&n);
+  char *address = strtok(NULL, " ");
+  swaddr_t addr = 0;
+  sscanf(address,"0x%x",&addr);
+  int i;
+  for(i = 0;i < n;i++)
+  {
+     if(i % 4 == 0)
+     printf("0x%08x: ",addr + 4*i);
+     printf("0x%08x ", swaddr_read(addr + 4*i, 4));
+     if((i + 1) % 4 == 0)
+     printf("\n");
+  }
+  if(n % 4 != 0)
+  printf("\n");
+
+  return 0;
+}
+
 void ui_mainloop() {
 	while(1) {
 		char *str = rl_gets();
