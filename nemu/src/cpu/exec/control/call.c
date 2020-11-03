c348cbda2d5beffbb386ff3a7c614e3847a19f4d
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 17:44:55 +0800

    >  compile NEMU 3019244287 zyh Linux zyh-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:44:55 up  6:59,  5 users,  load average: 0.08, 0.09, 0.04 8f4200e48cf07ae246cc2a7a755eeedc627f7525

---
+++ nemu/src/cpu/exec/control/call.c
@@ -0,0 +1,11 @@
+#include "cpu/exec/helper.h"
+
+make_helper(call_rel_32)
+{
+	int len = decode_si_l(eip + 1);
+	swaddr_t next_addr = eip + 1 + len;
+	swaddr_write(cpu.esp - 4,4,next_addr);
+	cpu.esp -= 4;
+	cpu.eip = next_addr + op_src->simm;
+	return len + 1;
+}
